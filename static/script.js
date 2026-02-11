21dc20f8a732274368fcae8a8687a4c036285639
arhangelartem479 <arhangelartem479@gmail.comgit>
2026-02-11 22:34:43 +1000

    Telegram WebApp integration

--- static/script.js
+++ static/script.js
@@ -1,15 +1,38 @@
+let userId = 12345; // fallback для браузера
+
+// Если открыто внутри Telegram
+if (window.Telegram && Telegram.WebApp) {
+    Telegram.WebApp.ready();
+    Telegram.WebApp.expand();
+
+    const user = Telegram.WebApp.initDataUnsafe.user;
+
+    if (user && user.id) {
+        userId = user.id;
+    }
+}
+
 async function openCase() {
     const img = document.getElementById("dropImg");
     img.classList.add("spin");
 
     setTimeout(async () => {
-        const res = await fetch("/open/12345");
+
+        const res = await fetch(`/open/${userId}`);
         const data = await res.json();
 
+        if (data.error) {
+            document.getElementById("result").innerHTML = "❌ " + data.error;
+            img.classList.remove("spin");
+            return;
+        }
+
         img.src = data.img;
+
         document.getElementById("result").innerHTML =
             "🎉 You won: " + data.name;
 
         img.classList.remove("spin");
-    }, 1000);
+
+    }, 1200);
 }
